e1a582b329d76dfc4cd3c1bb812dcf7ac7ce6183
Daniel Lopez <velezdev80@gmail.com>
2023-06-01 10:35:14 -0500

    add update animation activity registre

--- icecreamproject1/app/src/main/java/com/example/icecreamproject1/activities/RegistroUser.java
+++ icecreamproject1/app/src/main/java/com/example/icecreamproject1/activities/RegistroUser.java
@@ -3,14 +3,23 @@ package com.example.icecreamproject1.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.icecreamproject1.R;
 
 public class RegistroUser extends AppCompatActivity {
 
+    ImageView imgHelado1 = findViewById(R.id.imgVIce1);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_user);
+
+        Animation rotateAnim = AnimationUtils.loadAnimation(this, R.anim.rotacionimg);
+        imgHelado1.startAnimation(rotateAnim);
+
     }
 }
