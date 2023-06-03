105716a18fd556e89de50a9a4c976617f41b1c80
Daniel Lopez <velezdev80@gmail.com>
2023-06-03 18:21:33 -0500

    Forgot password section created

---
+++ icecreamproject1/app/src/main/java/com/example/icecreamproject1/activities/ForgotPassword.java
@@ -0,0 +1,16 @@
+package com.example.icecreamproject1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.icecreamproject1.R;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
