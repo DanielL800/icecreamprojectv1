5cd537c9ba4fbccabe361cac62f5f440c4946534
Daniel Lopez <velezdev80@gmail.com>
2023-06-25 21:08:09 -0500

    Creating activity main

--- icecreamproject1/app/src/main/java/com/example/icecreamproject1/activities/MainActivity.java
+++ icecreamproject1/app/src/main/java/com/example/icecreamproject1/activities/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.icecreamproject1.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 import com.example.icecreamproject1.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner spinner1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner1 = findViewById(R.id.spinnerP);
     }
 }
